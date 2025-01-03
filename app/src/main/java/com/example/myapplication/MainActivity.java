62a4e289ac802102265ac6df8dfd75588f9321b2
Sengeki1 <gamersengeki@gmail.com>
2025-01-03 13:40:26 -0100

    Dashboard UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView balanceAmount = findViewById(R.id.tvBalanceAmount);
+        TextView revenueAmount = findViewById(R.id.tvRevenueAmount);
+        TextView expenseAmount = findViewById(R.id.tvExpenseAmount);
+
+        Button btnAddRevenue = findViewById(R.id.btnAddRevenue);
+        Button btnAddExpense = findViewById(R.id.btnAddExpense);
+        Button btnViewTransactions = findViewById(R.id.btnViewTransactions);
+
+
     }
 }
